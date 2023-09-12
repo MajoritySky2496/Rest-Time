7342f4973647263beddbd598934037f063e67bc0
MajoritySky2496 <gzag2496@gmail.com>
2023-09-12 15:51:58 +0500

    hotelPage#1  Создал фрагмент  Создал viewModel  Создал layout  Добавил в navigation

---
+++ app/src/main/java/com/example/resttime/presentation/HotelPageFragment.kt
@@ -0,0 +1,21 @@
+package com.example.resttime.presentation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.resttime.databinding.PageHotelFragmentBinding
+import com.example.resttime.util.BindingFragment
+
+class HotelPageFragment:BindingFragment<PageHotelFragmentBinding>() {
+    override fun createBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?
+    ): PageHotelFragmentBinding {
+        return PageHotelFragmentBinding.inflate(inflater, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
