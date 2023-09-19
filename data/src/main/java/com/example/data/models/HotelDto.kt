a1a8e056ead5f777b3d14781adf9757749ce1e06
MajoritySky2496 <gzag2496@gmail.com>
2023-09-19 10:06:43 +0500

    hotelPage#2

---
+++ data/src/main/java/com/example/data/models/HotelDto.kt
@@ -0,0 +1,10 @@
+package com.example.data.models
+
+data class HotelDto(
+    val id:String,
+    val adress:String,
+    val minimal_price:Int,
+    val rating:Int,
+    val rating_name:String,
+    val image_urls:String
+)
